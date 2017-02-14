d826bc760152c763a6cb5383816b61ec74c91ddd
AnidaKhizar <anida-khizar@hotmail.fr>
2017-02-14 20:50:26 +0100

    correction of the laplacian matrix and y_values

--- main.py
+++ main.py
@@ -1,40 +1,52 @@
+"""
+File : main.py
+[MAIN3 - Schrodinger] Feb. - June 2017
+A. Khizar, R. Ndiaye, V. Nicol, M. Pecheux
+Referent: F. Aviat
+
+Main file that represents the wave function using the TISE.
+"""
+
+# useful imports
 import numpy as np
 import matplotlib.pyplot as plt
 from math import sin, pi
 
+
 # general variables
-x_min = 0
-x_max = 100
-n = 100       # number of subdivisions
+x_min = -5.
+x_max = 5.
+n = 100        # number of subdivisions of the space
+delta_x = (x_max - x_min) / n
+
+# we make the space discrete
+x_values = np.arange(x_min, x_max, delta_x)
 
 # declaration of main matrices
-laplacian = -2*np.eye(n) + np.diag(np.ones(n - 1), -1) + np.diag(np.ones(n - 1), 1)
-V = np.zeros(n)     # null potential
-H = laplacian + V
+laplacian = -6.64E-34 ** 2 / (2 * delta_x**2) * (-2*np.eye(n) + np.diag(np.ones(n - 1), -1) + np.diag(np.ones(n - 1), 1))
+V0 = np.zeros(n)                                # null potential
+V1 = np.asarray([x ** 2 for x in x_values])     # harmonic potential
+# here you choose which potential you add:
+H = laplacian + V0
 # we get the eigen vectors of the H matrix
 # to get the eigen functions of the H operator
 eigen_values, eigen_vectors = np.linalg.eigh(H)
 
-# we make the space discrete
-x_values = np.arange(x_min, x_max, (x_max - x_min) / n)
-
-# graphic representation
-for i in range(1, 3):
+# graphic representation of the first modes
+for i in range(0, 4):
     # we get the right psi values = the corresponding eigen vector
     # of the H matrix
-    """y_values = eigen_vectors[0]
-    for j in range(1, len(eigen_vectors)):
-        if j < i:
-            y_values += eigen_vectors[j]"""
 
-    y_values = eigen_vectors[i]
-    #y_values = [sum(x) for x in zip(*eigen_vectors)]
-    #y_values = [sin(pi*i*x/x_max) for x in x_values]
-    # we plot the psi function against the x values
-    plt.plot(x_values, y_values, label='i = %d' % i)
+    # THE PROBLEM WAS: the eigen vectors are COLUMN VECTORS!
+    y_values = eigen_vectors[:,i]           # using the H eigen vectors
 
-# we finish up the graph and show it
+    # we plot the result against the x values
+    plt.figure(1)
+    plt.plot(x_values, y_values, label='mode %d' % (i+1))
+
+# we finish up the graphs and show them
 plt.legend()
 plt.xlabel('Horizontal position')
 plt.ylabel('Wave functions')
+
 plt.show()
